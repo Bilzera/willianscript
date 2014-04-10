8faebb9fd96a0ef139af8eb9570719976df2a7da
tahzanin <zaninthalita@gmail.com>
2014-04-10 17:08:39 -0300

    LOGIN COM O GOOGLE

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -2,10 +2,14 @@
 from __future__ import absolute_import, unicode_literals
 import tmpl_middleware
 from tekton.gae.middleware import router_middleware, parameter, webapp2_dependencies, email_errors
+from usuario import LogInOut_middleware
 
 SENDER_EMAIL = 'willian.augusto.campos@gmail.com'
 WEB_BASE_PACKAGE = "web"
-MIDDLEWARES = [tmpl_middleware.execute, email_errors.execute, webapp2_dependencies.execute, parameter.execute,
+MIDDLEWARES = [LogInOut_middleware.execute,
+               tmpl_middleware.execute,
+               email_errors.execute,
+               webapp2_dependencies.execute,
+               parameter.execute,
                router_middleware.execute]
 
-[]
